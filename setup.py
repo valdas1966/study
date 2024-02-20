f385af17cfa3636e501bbe6ffea2371347354167
valdas1966 <72308177+valdas1966@users.noreply.github.com>
2024-02-20 13:42:55 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Study GitHub',
+    version='0.1',
+    packages=find_packages(),
+    description='Study GitHub',
+    author='VALDAS',
+    author_email='valdas@gmail.com',
+    url='https://github.com/valdas1966/study',
+    install_requires=[
+        # Any dependencies your project needs, e.g., 'requests>=2.25.1'
+    ]
+)
